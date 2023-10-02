6171b545e34dcbaad38aed58644047187398c69b
dankel23 <125362327+dankel23@users.noreply.github.com>
2023-10-02 08:44:23 -0300

    2

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore} from 'firebase/firestore';
 
 //Aqui vc deve colocar as suas credenciais 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyBX2nkIZr76FOEjTqyncfqfcu9q_x0Ly0U",
+  authDomain: "danielfal513.firebaseapp.com",
+  projectId: "danielfal513",
+  storageBucket: "danielfal513.appspot.com",
+  messagingSenderId: "866872135206",
+  appId: "1:866872135206:web:526b514f9f2478b560a0e7",
+  measurementId: "G-53JD7J5RR7"
 };
 
 const app = initializeApp(firebaseConfig);
